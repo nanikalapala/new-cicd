aee6ec958655a6b8a33342e7f8e2464b956892e3
root <root@ip-172-31-46-59.ap-southeast-2.compute.internal>
2026-06-18 08:47:45 +0000

    Added member management

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
+const members = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -33,6 +34,85 @@ app.post("/login", (req, res) => {
         res.send("Invalid Credentials");
 
     }
+
+});
+
+app.get("/add-member",(req,res)=>{
+
+    res.sendFile(
+        path.join(
+            __dirname,
+            "public",
+            "add-member.html"
+        )
+    );
+
+});
+
+app.post("/add-member",(req,res)=>{
+
+    members.push(req.body);
+
+    res.send(`
+        <h1>Member Added Successfully</h1>
+
+        <a href="/members">
+            View Members
+        </a>
+    `);
+
+});
+
+app.get("/members",(req,res)=>{
+
+    let html = `
+    <html>
+    <head>
+    <title>Members</title>
+    </head>
+    <body>
+
+    <h1>Insurance Members</h1>
+
+    <table border="1" cellpadding="10">
+
+    <tr>
+        <th>Name</th>
+        <th>Mobile</th>
+        <th>Age</th>
+        <th>Gender</th>
+        <th>Relationship</th>
+    </tr>
+    `;
+
+    members.forEach(member => {
+
+        html += `
+        <tr>
+            <td>${member.name}</td>
+            <td>${member.mobile}</td>
+            <td>${member.age}</td>
+            <td>${member.gender}</td>
+            <td>${member.relationship}</td>
+        </tr>
+        `;
+
+    });
+
+    html += `
+    </table>
+
+    <br>
+
+    <a href="/dashboard.html">
+        Dashboard
+    </a>
+
+    </body>
+    </html>
+    `;
+
+    res.send(html);
 
 });
 
